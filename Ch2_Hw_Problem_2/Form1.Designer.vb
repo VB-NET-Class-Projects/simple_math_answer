38b31bc71810ae837ec58095283ebaea4feb9e94
Henri-webdev <henri.webdev@gmail.com>
2020-05-20 16:42:37 -0500

    Initial commit

---
+++ Ch2_Hw_Problem_2/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnShowAnswer = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.lblProblem = New System.Windows.Forms.Label()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnShowAnswer
+        '
+        Me.btnShowAnswer.AutoSize = True
+        Me.btnShowAnswer.Location = New System.Drawing.Point(240, 204)
+        Me.btnShowAnswer.Name = "btnShowAnswer"
+        Me.btnShowAnswer.Size = New System.Drawing.Size(82, 23)
+        Me.btnShowAnswer.TabIndex = 0
+        Me.btnShowAnswer.Text = "Show Answer"
+        Me.btnShowAnswer.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(348, 204)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(51, 23)
+        Me.btnClose.TabIndex = 1
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'lblProblem
+        '
+        Me.lblProblem.AutoSize = True
+        Me.lblProblem.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblProblem.Location = New System.Drawing.Point(225, 159)
+        Me.lblProblem.Name = "lblProblem"
+        Me.lblProblem.Size = New System.Drawing.Size(141, 25)
+        Me.lblProblem.TabIndex = 2
+        Me.lblProblem.Text = "40(3)+20-3 = "
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.AutoSize = True
+        Me.lblAnswer.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAnswer.Location = New System.Drawing.Point(362, 159)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(24, 25)
+        Me.lblAnswer.TabIndex = 3
+        Me.lblAnswer.Text = "?"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.lblProblem)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnShowAnswer)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnShowAnswer As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents lblProblem As Label
+    Friend WithEvents lblAnswer As Label
+End Class
---
+++ Ch2_Hw_Problem_2/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btnShowAnswer_Click(sender As Object, e As EventArgs) Handles btnShowAnswer.Click
+        lblAnswer.Text = "137"
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+End Class
